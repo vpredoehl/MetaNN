a4f81c74e6f4105f2ad10c969551b4dd3d60dfd4
Vincent Predoehl <vpredoehl@icloud.com>
2025-12-13 00:28:49 -0600

    - added cpp_run_matmul_add_demo to test MetalLib

--- Tests/LayerTest/main.cpp
+++ Tests/LayerTest/main.cpp
@@ -2,8 +2,11 @@
 #include <principal/_.h>
 #include <recurrent/_.h>
 
+#include "MatMulAddDemo.h"
+
 int main()
 {
+    cpp_run_matmul_add_demo();
     Test::Layer::test_composite();
     Test::Layer::test_principal();
     Test::Layer::test_recurrent();
