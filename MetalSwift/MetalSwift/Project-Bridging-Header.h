1703bf63d69ecf4e690dba868c45f519b39c9d24
Vincent Predoehl <vpredoehl@icloud.com>
2025-12-13 16:52:14 -0600

    - remove MetalSwift subtree

--- MetalSwift/MetalSwift/Project-Bridging-Header.h
+++
@@ -1,4 +0,0 @@
-// Use this bridging header to expose Objective-C(++) headers to Swift.
-// In Build Settings, set "Objective-C Bridging Header" to the relative path of this file.
-
-#import "CppBridge.h"
