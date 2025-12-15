c95de938874afb6da4f28ae2bc9782e67798d4e0
Vincent Predoehl <vpredoehl@icloud.com>
2025-12-14 20:58:06 -0600

    - OtherTests compiles

--- Tests/OtherTests/main.cpp
+++ Tests/OtherTests/main.cpp
@@ -1,8 +1,45 @@
-#include <model/_.h>
-#include <policies/_.h>
+#include <iostream>
+
+// Forward declarations of test entry points found in the project
+namespace Test {
+namespace Data { void test_tensor(); }
+namespace Layer {
+namespace Principal { 
+    void test_add_layer();
+    void test_param_source_layer();
+}
+namespace Composite {
+    void test_compose_kenrel();
+}
+}
+namespace Operation {
+namespace Math {
+    void test_cos();
+    void test_cos_grad();
+}
+}
+}
 
 int main(int argc, char **argv)
 {
-    Test::test_model();
-    Test::Policies::Test();
+    using std::cout; using std::endl;
+
+    cout << "Running MetaNN tests..." << endl;
+
+    // Data tests
+    Test::Data::test_tensor();
+
+    // Layer principal tests
+    Test::Layer::Principal::test_add_layer();
+    Test::Layer::Principal::test_param_source_layer();
+
+    // Layer composite tests
+    Test::Layer::Composite::test_compose_kenrel();
+
+    // Operation math tests
+    Test::Operation::Math::test_cos();
+    Test::Operation::Math::test_cos_grad();
+
+    cout << "All selected tests finished." << endl;
+    return 0;
 }
